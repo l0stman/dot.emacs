ea3825fd3d944cc1f4828adb26f152714bb7c414
l0stman <endian.sign@gmail.com>
2010-01-11 17:41:12 +0300

    Track some files.

---
+++ emacs-custom.el
@@ -0,0 +1,14 @@
+;;;; Emacs customization file.
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(quack-fontify-style (quote emacs))
+ '(quack-newline-behavior (quote indent-newline-indent)))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
