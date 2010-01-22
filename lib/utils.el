24bfbf26a7a43c3e6d370b9dc21f0716db83bd0f
l0stman <endian.sign@gmail.com>
2010-01-22 11:37:53 +0300

    Using destructuring bindings in the loop macros in utils.

--- lib/utils.el
+++ lib/utils.el
@@ -5,20 +5,18 @@
 functions in the key mapping map (the global one if null)."
   (eval-after-load "cl"
     '`(progn
-	,@(loop for plist on bindings by #'cddr
-		collect (let ((key (car plist)) (fn (cadr plist)))
-			  (if map
-			      `(define-key ,map (kbd ,key) ',fn)
-			    `(global-set-key (kbd ,key) ',fn)))))))
+	,@(loop for (key fn) on bindings by #'cddr
+		collect (if map
+                            `(define-key ,map (kbd ,key) ',fn)
+                          `(global-set-key (kbd ,key) ',fn))))))
 
 (defmacro subskeys (map &rest funcs)
   "Replace in the keymap map the old definitions of functions
 with new ones represented as property list."
   (eval-after-load "cl"
     '`(progn
-        ,@(loop for plist on funcs by #'cddr
-                collect `(substitute-key-definition
-                          ',(car plist) ',(cadr plist) ,map)))))
+        ,@(loop for (new old) on funcs by #'cddr
+                collect `(substitute-key-definition ',new ',old ,map)))))
 
 (defun symb (&rest args)
   "Produce a symbol from the lisp objects."
