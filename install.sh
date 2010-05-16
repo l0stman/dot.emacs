e4bae1103f283ec034431261313e79e6fa19888d
l0stman <endian.sign@gmail.com>
2010-05-16 19:26:20 +0300

    Determine the emacs version number to find the slime directory.

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -x
 
+emacs_version ()
+{
+    emacs --version | head -1 | sed -E 's/GNU Emacs ([0-9]+\.[0-9]).*/\1/'
+}
+
 EMACSDIR=$HOME/.emacs.d
-SLIME="/usr/local/share/emacs/23.1/site-lisp/slime"
+SLIME="/usr/local/share/emacs/`emacs_version`/site-lisp/slime"
 CORE="${EMACSDIR}/sbcl.core-with-swank"
 
 cp xmodmaprc ~/.xmodmaprc
