4d1f06c291236f2864609dbe99cb147ee40b81dd
l0stman <endian.sign@gmail.com>
2010-11-03 12:28:39 +0300

    Change the documentation directory from plt to racket.

--- emacs-custom.el
+++ emacs-custom.el
@@ -7,7 +7,7 @@
  '(help-char 63)
  '(quack-fontify-style (quote emacs))
  '(quack-newline-behavior (quote indent-newline-indent))
- '(quack-pltcollect-dirs (quote ("/usr/local/share/plt" "/usr/local/lib/plt/collects")))
+ '(quack-pltcollect-dirs (quote ("/usr/local/share/racket" "/usr/local/lib/racket/collects")))
  '(quack-programs (quote ("loot" "bigloo" "csi" "csi -hygienic" "gosh" "gsi" "gsi ~~/syntax-case.scm -" "guile" "kawa" "mit-scheme" "mred -z" "mzscheme" "mzscheme -il r6rs" "mzscheme -il typed-scheme" "mzscheme -M errortrace" "mzscheme3m" "mzschemecgc" "rs" "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi"))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
